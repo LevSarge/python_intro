6132b65158cc8d17b4d15821e62701c7db5994a3
LevSarge <levimsargeant@gmail.com>
2021-12-02 20:52:19 -0700

    Finished assignment

---
+++ data_presenter.py
@@ -0,0 +1,22 @@
+data = open("CupcakeInvoices.csv")
+
+#for line in data:
+#    print(line)
+
+#for line in data:
+#        cupcakes = line.split(',')
+#        print(cupcakes[2])
+
+#for line in data:
+#    invoices = line.split(',')
+#    total = int(invoices[3]) * float(invoices[4])
+ #   print(total)
+
+total = 0
+
+for line in data:
+    values = line.split(',')
+    total = total + (int(values[3]) * float(values[4]))
+
+print(total)
+#log_file.close()
